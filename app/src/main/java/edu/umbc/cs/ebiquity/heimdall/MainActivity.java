275821f58e8f243ba1fb71acdf419aa157f0bd61
Prajit Kumar Das <prajitdas@gmail.com>
2015-11-08 15:07:27 -0500

    added logic in Heimdall

--- app/src/main/java/edu/umbc/cs/ebiquity/heimdall/MainActivity.java
+++ app/src/main/java/edu/umbc/cs/ebiquity/heimdall/MainActivity.java
@@ -5,24 +5,48 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+    private Switch policy1Switch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        policy1Switch = (Switch) findViewById(R.id.policy1Switch);
+        policy1Switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    String policy1;
+                    System.setProperty("POLICY1", "true");
+                    policy1 = System.getProperty("POLICY1");
+                    Log.i("PKDLog", "POLICY1: " + policy1);
+                } else {
+                    String policy1;
+                    System.setProperty("POLICY1", "false");
+                    policy1 = System.getProperty("POLICY1");
+                    Log.i("PKDLog", "POLICY1: " + policy1);
+                }
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "New policy addition is disabled right now!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
